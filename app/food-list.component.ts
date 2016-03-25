1f7a18689d430e3925f9811eeb2fbed038ded8fd
Guest User <student@epicodus.com>
2016-03-25 08:12:04 -0700

    Add ability to select and highlight a specific food.

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -4,11 +4,13 @@ import { Food } from './food.model';
 @Component({
   selector: "food-list",
   inputs: ['foodList'],
-  outputs: [],
+  outputs: ['onFoodSelect'],
   directives: [],
   pipes: [],
   template: `
-  <div *ngFor="#currentFood of foodList">
+  <div *ngFor="#currentFood of foodList"
+  (click)="foodClicked(currentFood)"
+  [class.selected]='currentFood === selectedFood'>
     <h4> {{ currentFood.name }} </h4>
     <h4> {{ currentFood.details }} </h4>
     <h4> {{ currentFood.calories }} </h4>
@@ -18,4 +20,14 @@ import { Food } from './food.model';
 
 export class FoodListComponent {
   public foodList: Food[];
+  public onFoodSelect: EventEmitter<Food>;
+  public selectedFood: Food;
+
+  constructor(){
+    this.onFoodSelect = new EventEmitter();
+  }
+  foodClicked(clickedFood: Food): void {
+    this.selectedFood = clickedFood;
+    this.onFoodSelect.emit(clickedFood);
+  }
 }
